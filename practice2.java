7f18a2da795bea330afefd5afc82b8f5ecbae43a
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-10-22 20:47:17 +0900

    [java]

---
+++ practice2.java
@@ -0,0 +1,21 @@
+//()の中身を出力せよという命令
+System.out.println("Hello world");
+
+class Main {
+    public static void main(String[]args){
+
+    }
+}
+
+class Main {
+  public static void main(String[] args) {
+    // 数値の17を出力してください
+    System.out.println(17);
+    
+    // 5に3を足した値を出力してください
+      System.out.println(5 + 3);
+    
+    // 「5 + 3」を文字列として出力してください
+       System.out.println("5 + 3");
+  }
+}
