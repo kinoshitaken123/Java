d31ab457cf2ba7426490e0f6fbb961e90d231fba
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-10-23 17:59:41 +0900

    [java]

--- practice2.java
+++ practice2.java
@@ -19,3 +19,47 @@ class Main {
        System.out.println("5 + 3");
   }
 }
+
+class Main {            
+  public static void main(String[] args) {            
+    // int型の変数numberを定義してください            
+              int number;            
+                
+
+    // 変数numberに3を代入してください            
+               number = 3;            
+                
+
+    // 変数numberを出力してください            
+                System.out.println(number);            
+
+    // String型の変数nameを定義してください            
+               String name;            
+                
+
+    // 変数nameに"Wanko"を代入してください            
+                name = "Wanko";            
+                
+
+    // 変数nameを出力してください            
+                System.out.println(name);            
+                
+
+  }            
+}
+
+//更新時の注意
+int number = 3;
+number = 5;
+
+//自己代入
+//変数xに3が入っているとき、xの値に2を足して5に上書きしたい場合はどうすればよいでしょうか。
+//この場合は、左の図のように変数xと2を足して、また変数xに代入します。不思議な式に見えますが、
+//プログラミングでは「=」は代入なので問題はありません。よく使うので覚えておきましょう。
+//またこのような代入を自己代入と呼びます
+
+int x = 3;
+System.out.println(x);
+x = x +2;
+
+System.out.println(x);
