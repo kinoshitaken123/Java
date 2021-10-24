1111b382c3aa9339bcb4a49055ac1d7c39956456
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-10-24 21:53:54 +0900

    [メソッドオーバーロード]

--- progate3.java
+++ progate3.java
@@ -59,3 +59,53 @@ class Main {
       
     }
   }
+
+  class Main {
+    public static void main(String[] args) {
+      // fullNameメソッドの結果を変数nameに代入してください
+      String name = fullName("Kate","Jones");
+      
+      // printDataの引数を書き換えてください
+      printData(name, 27);
+      
+      // こちらは書き換えないでください
+      printData("John Christopher Smith", 65);
+      
+    }
+  
+    public static void printData(String name, int age) {
+      System.out.println("私の名前は" + name + "です");
+      System.out.println("年齢は" + age + "歳です");
+    }
+  
+    // fullNameメソッドを定義してください
+      public static String fullName(String firstName, String lastName) {            
+      return firstName + " " + lastName;            
+    }
+    
+  }
+
+  //メソッドオーバーロード
+  class Main {
+    public static void main(String[] args) {
+      printData(fullName("Kate", "Jones"), 27);
+      
+      // fullNameメソッドを用いて、printDataの引数を書き換えてください
+      printData(fullName("John", "Christopher", "Smith"), 65);
+    }
+  
+    public static void printData(String name, int age) {
+      System.out.println("私の名前は" + name + "です");
+      System.out.println("年齢は" + age + "歳です");
+    }
+  
+    public static String fullName(String firstName, String lastName) {
+      return firstName + " " + lastName;
+    }
+    
+    // fullNameメソッドを定義してください
+    public static String fullName(String firstName, String middleName, String lastName){
+    return firstName + " " + middleName + " " + lastName;
+    }
+    
+  }
