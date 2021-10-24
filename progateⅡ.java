f9e2aa8be76652d6d8622b41dd30039fd13cd5d5
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-10-24 16:44:50 +0900

    [繰り返し処理]

--- progateⅡ.java
+++ progateⅡ.java
@@ -187,3 +187,15 @@ class Main {
       }
     }
   }
+
+  //while
+  class Main {
+    public static void main(String[] args) {
+      int i =1;
+
+       while (i <= 5){
+        System.out.println(i);
+         i++
+       }
+    }
+   }
