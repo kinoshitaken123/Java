8c00404e639bd866254bad889c09c44fbd9621e9
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-10-24 20:44:02 +0900

    [java]

---
+++ progate3.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+      hello();
+    }
+    
+    public static void hello() {
+      // "Hello World"を、"Hello Java"に書き換えてください
+      System.out.println("Hello World");
+    }
+  }
